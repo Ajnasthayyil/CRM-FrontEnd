344fc7ed6f13bc848a3d4cd585fb7c626c6c0089
Ajnas T <ajnasthayyil123@gmail.com>
2026-07-23 11:22:26 +0530

    Generate some of Layout Components

--- src/app/layouts/layouts.module.ts
+++ src/app/layouts/layouts.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AuthLayoutComponent } from './auth/auth-layout/auth-layout.component';
+import { DashboardLayoutComponent } from './dashboard/dashboard-layout/dashboard-layout.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AuthLayoutComponent,
+    DashboardLayoutComponent
+  ],
   imports: [
     CommonModule
   ]
